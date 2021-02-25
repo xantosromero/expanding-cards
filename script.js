214865e5d7590d57af851f9df5ab354a5ac436be
Santos Romero <xantosromero@gmail.com>
2021-02-25 14:11:43 -0500

    primer commit
    Archivos finalizados

---
+++ script.js
@@ -0,0 +1,24 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+
+
+/*
+
+Cambiar las imágenes
+Redactar títulos de las imágenes
+Agregar contenido propio de valor
+
+*/
